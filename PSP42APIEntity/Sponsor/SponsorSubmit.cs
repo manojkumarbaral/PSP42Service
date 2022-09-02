012324b3368c95ced85d4bfa292f493907d636db
MANOJ KUMAR BARAL <111396548+manojkumarbaral@users.noreply.github.com>
2022-09-02 01:48:04 -0700

    Add files via upload

---
+++ PSP42APIEntity/Sponsor/SponsorSubmit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessEntities.Sponsor
+{
+    public class SponsorSubmit
+    {
+        public string SponsorEmail { get; set; }
+        public string Code { get; set; }
+        public string SponsorMobile { get; set; }
+        public string FullName { get; set; }
+        public string EIDNumber { get; set; }
+        public string UIDNumber { get; set; }
+        public int? CreatedBy_UID { get; set; }
+        public string customerCode { get; set; }
+    }
+}
